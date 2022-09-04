e0f88ed2a2bb1e1cc797b0004525b659df39a89f
truongct <tientruongcao51@gmail.com>
2022-09-05 01:32:39 +0700

    update main.py

--- create-cluster.sh
+++ create-cluster.sh
@@ -3,44 +3,31 @@
 # Variables
 resourceGroup="acdnd-c4-project"
 clusterName="udacity-cluster"
+vmSize="Standard_D2plds_v5"
 
 # Install aks cli
 echo "Installing AKS CLI"
 
-sudo az aks install-cli
+az aks install-cli
 
 echo "AKS CLI installed"
 
 # Create AKS cluster
-echo "Step 1 - Creating AKS cluster $clusterName"
-# Use either one of the "az aks create" commands below
-# For users working in their personal Azure account
-# This commmand will not work for the Cloud Lab users, because you are not allowed to create Log Analytics workspace for monitoring
+echo "Creating AKS cluster $clusterName"
+
 az aks create \
 --resource-group $resourceGroup \
 --name $clusterName \
 --node-count 1 \
 --enable-addons monitoring \
---generate-ssh-keys
-
-# For Cloud Lab users
-az aks create \
---resource-group $resourceGroup \
---name $clusterName \
---node-count 1 \
---generate-ssh-keys
-
-# For Cloud Lab users
-# This command will is a substitute for "--enable-addons monitoring" option in the "az aks create"
-# Use the log analytics workspace - Resource ID
-# For Cloud Lab users, go to the existing Log Analytics workspace --> Properties --> Resource ID. Copy it and use in the command below.
-az aks enable-addons -a monitoring -n $clusterName -g $resourceGroup --workspace-resource-id "/subscriptions/6c39f60b-2bb1-4e37-ad64-faaf30beaca4/resourcegroups/cloud-demo-153430/providers/microsoft.operationalinsights/workspaces/loganalytics-153430"
+--generate-ssh-keys \
+--node-vm-size $vmSize
 
 echo "AKS cluster created: $clusterName"
 
 # Connect to AKS cluster
 
-echo "Step 2 - Getting AKS credentials"
+echo "Getting AKS credentials"
 
 az aks get-credentials \
 --resource-group $resourceGroup \
@@ -51,6 +38,6 @@ echo "Verifying connection to $clusterName"
 
 kubectl get nodes
 
-# echo "Deploying to AKS cluster"
-# The command below will deploy a standard application to your AKS cluster. 
-# kubectl apply -f azure-vote.yaml
+echo "Deploying to AKS cluster"
+
+kubectl apply -f azure-vote.yaml
